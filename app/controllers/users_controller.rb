28ef65d83eb6384581c36028df471baec43945c3
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-05-17 11:37:33 +0200

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,12 @@ class UsersController < ApplicationController
 
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: FILL_IN).(paginate(page: params[:page])
   end
 
   def show
     @user = User.find params[:id]
+    redirect_to root_url and return unless FILL_IN
   end
 
   def new
@@ -21,9 +22,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new params_for_user    
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+
     else
       render :new
     end
